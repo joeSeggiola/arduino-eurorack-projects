89d6648c254ec6fa74768d64f49d4d08cbba7383
joeSeggiola <joeseggiola@gmail.com>
2021-06-27 14:21:03 +0200

    Updated MCP4728 tools

---
+++ tools/mcp4728_addr/MCP4728.cpp
@@ -0,0 +1,214 @@
+
+// LIBRARY FOR MCP4728 
+// Link: https://github.com/hideakitai/MCP4728
+// Author: Hideaki Tai
+// License: MIT (https://github.com/hideakitai/MCP4728/blob/master/LICENSE)
+// Extended by Joe Seggiola to include optional LDAC
+
+#pragma once
+#ifndef MCP4728_H
+#define MCP4728_H
+
+#include "Arduino.h"
+#include <Wire.h>
+
+class MCP4728 {
+	
+	public:
+
+		enum class CMD {
+			FAST_WRITE = 0x00,
+			MULTI_WRITE = 0x40,
+			SINGLE_WRITE = 0x58,
+			SEQ_WRITE = 0x50,
+			SELECT_VREF = 0x80,
+			SELECT_GAIN = 0xC0,
+			SELECT_PWRDOWN = 0xA0
+		};
+		
+		enum class VREF { VDD, INTERNAL_2_8V };
+		enum class PWR_DOWN { NORMAL, GND_1KOHM, GND_100KOHM, GND_500KOHM };
+		enum class GAIN { X1, X2 };
+
+		void init(TwoWire& w, uint8_t addr = 0, int8_t pin = -1) {
+			wire_ = &w;
+			addr_ = I2C_ADDR + addr;
+			pin_ldac_ = pin;
+			if (pin_ldac_ > -1) {
+				pinMode(pin_ldac_, OUTPUT);
+				enable(false);
+			}
+			readRegisters();
+		}
+		
+		void enable(bool b) {
+			if (pin_ldac_ > -1) {
+				digitalWrite(pin_ldac_, !b);
+			}
+		}
+
+		uint8_t analogWrite(uint8_t ch, uint16_t data, bool b_eep = false) {
+			if (b_eep) {
+				eep_[ch].data = data > 0xFFF ? 0xFFF : data;
+				return singleWrite(ch);
+			} else {
+				reg_[ch].data = data > 0xFFF ? 0xFFF : data;
+				return fastWrite();
+			}
+		}
+
+		uint8_t analogWrite(uint16_t a, uint16_t b, uint16_t c, uint16_t d, bool b_eep = false) {
+			if (b_eep) {
+				reg_[0].data = eep_[0].data = a > 0xFFF ? 0xFFF : a;
+				reg_[1].data = eep_[1].data = b > 0xFFF ? 0xFFF : b;
+				reg_[2].data = eep_[2].data = c > 0xFFF ? 0xFFF : c;
+				reg_[3].data = eep_[3].data = d > 0xFFF ? 0xFFF : d;
+				return seqWrite();
+			} else {
+				reg_[0].data = a > 0xFFF ? 0xFFF : a;
+				reg_[1].data = b > 0xFFF ? 0xFFF : b;
+				reg_[2].data = c > 0xFFF ? 0xFFF : c;
+				reg_[3].data = d > 0xFFF ? 0xFFF : d;
+				return fastWrite();
+			}
+		}
+
+		uint8_t selectVref(VREF a, VREF b, VREF c, VREF d) {
+			reg_[0].vref = a;
+			reg_[1].vref = b;
+			reg_[2].vref = c;
+			reg_[3].vref = d;
+			uint8_t data = (uint8_t)CMD::SELECT_VREF;
+			for (uint8_t i = 0; i < 4; ++i) bitWrite(data, 3 - i, (uint8_t)reg_[i].vref);
+			wire_->beginTransmission(addr_);
+			wire_->write(data);
+			return wire_->endTransmission();
+		}
+
+		uint8_t selectPowerDown(PWR_DOWN a, PWR_DOWN b, PWR_DOWN c, PWR_DOWN d) {
+			reg_[0].pd = a;
+			reg_[1].pd = b;
+			reg_[2].pd = c;
+			reg_[3].pd = d;
+			uint8_t h = ((uint8_t)CMD::SELECT_PWRDOWN) | ((uint8_t)a << 2) | (uint8_t)b;
+			uint8_t l = 0 | ((uint8_t)c << 6) | ((uint8_t)d << 4);
+			wire_->beginTransmission(addr_);
+			wire_->write(h);
+			wire_->write(l);
+			return wire_->endTransmission();
+		}
+
+		uint8_t selectGain(GAIN a, GAIN b, GAIN c, GAIN d) {
+			reg_[0].gain = a;
+			reg_[1].gain = b;
+			reg_[2].gain = c;
+			reg_[3].gain = d;
+			uint8_t data = (uint8_t)CMD::SELECT_GAIN;
+			for (uint8_t i = 0; i < 4; ++i) bitWrite(data, 3 - i, (uint8_t)reg_[i].gain);
+			wire_->beginTransmission(addr_);
+			wire_->write(data);
+			return wire_->endTransmission();
+		}
+		
+		void readRegisters() {
+			wire_->requestFrom((int)addr_, 24);
+			if (wire_->available() == 24) {
+				for (uint8_t i = 0; i < 8; ++i) {
+					uint8_t data[3];
+					bool isEeprom = i % 2;
+					for (uint8_t i = 0; i < 3; ++i) data[i] = wire_->read();
+					uint8_t ch = (data[0] & 0x30) >> 4;
+					if (isEeprom) {
+						read_eep_[ch].vref = (VREF)	((data[1] & 0b10000000) >> 7);
+						read_eep_[ch].pd   = (PWR_DOWN)((data[1] & 0b01100000) >> 5);
+						read_eep_[ch].gain = (GAIN)	((data[1] & 0b00010000) >> 4);
+						read_eep_[ch].data = (uint16_t)((data[1] & 0b00001111) << 8 | data[2]);
+					} else {
+						read_reg_[ch].vref = (VREF)	((data[1] & 0b10000000) >> 7);
+						read_reg_[ch].pd   = (PWR_DOWN)((data[1] & 0b01100000) >> 5);
+						read_reg_[ch].gain = (GAIN)	((data[1] & 0b00010000) >> 4);
+						read_reg_[ch].data = (uint16_t)((data[1] & 0b00001111) << 8 | data[2]);
+					}
+				}
+			}
+		}
+
+		uint8_t getVref(uint8_t ch, bool b_eep = false) { 
+			return b_eep ? (uint8_t)read_eep_[ch].vref : (uint8_t)read_reg_[ch].vref; 
+		}
+		
+		uint8_t getGain(uint8_t ch, bool b_eep = false) { 
+			return b_eep ? (uint8_t)read_eep_[ch].gain: (uint8_t)read_reg_[ch].gain; 
+		}
+		
+		uint8_t getPowerDown(uint8_t ch, bool b_eep = false) { 
+			return b_eep ? (uint8_t)read_eep_[ch].pd : (uint8_t)read_reg_[ch].pd; 
+		}
+		
+		uint16_t getDACData(uint8_t ch, bool b_eep = false) { 
+			return b_eep ? (uint16_t)read_eep_[ch].data : (uint16_t)read_reg_[ch].data; 
+		}
+		
+	private:
+		
+		uint8_t fastWrite() {
+			wire_->beginTransmission(addr_);
+			for (uint8_t i = 0; i < 4; ++i) {
+				wire_->write((uint8_t)CMD::FAST_WRITE | highByte(reg_[i].data));
+				wire_->write(lowByte(reg_[i].data));
+			}
+			return wire_->endTransmission();
+		}
+
+		uint8_t multiWrite() {
+			wire_->beginTransmission(addr_);
+			for (uint8_t i = 0; i < 4; ++i) {
+				wire_->write((uint8_t)CMD::MULTI_WRITE | (i << 1));
+				wire_->write(((uint8_t)reg_[i].vref << 7) | ((uint8_t)reg_[i].pd << 5) | ((uint8_t)reg_[i].gain << 4) | highByte(reg_[i].data));
+				wire_->write(lowByte(reg_[i].data));
+			}
+			return wire_->endTransmission();
+		}
+
+		uint8_t seqWrite() {
+			wire_->beginTransmission(addr_);
+			wire_->write((uint8_t)CMD::SEQ_WRITE);
+			for (uint8_t i = 0; i < 4; ++i) {
+				wire_->write(((uint8_t)eep_[i].vref << 7) | ((uint8_t)eep_[i].pd << 5) | ((uint8_t)eep_[i].gain << 4) | highByte(eep_[i].data));
+				wire_->write(lowByte(eep_[i].data));
+			}
+			return wire_->endTransmission();
+		}
+
+		uint8_t singleWrite(uint8_t ch) {
+			wire_->beginTransmission(addr_);
+			wire_->write((uint8_t)CMD::SINGLE_WRITE | (ch << 1));
+			wire_->write(((uint8_t)eep_[ch].vref << 7) | ((uint8_t)eep_[ch].pd << 5) | ((uint8_t)eep_[ch].gain << 4) | highByte(eep_[ch].data));
+			wire_->write(lowByte(eep_[ch].data));
+			return wire_->endTransmission();
+		}
+
+	private:
+
+		struct DACInputData {
+			VREF vref;
+			PWR_DOWN pd;
+			GAIN gain;
+			uint16_t data;
+		};
+
+		const uint8_t I2C_ADDR {0x60};
+
+		uint8_t addr_ {I2C_ADDR};
+		int8_t pin_ldac_;
+
+		DACInputData reg_[4];
+		DACInputData eep_[4];
+		DACInputData read_reg_[4];
+		DACInputData read_eep_[4];
+		
+		TwoWire* wire_;
+	
+};
+
+#endif
--- tools/mcp4728_addr/mcp4728_addr.ino
+++ tools/mcp4728_addr/mcp4728_addr.ino
@@ -18,53 +18,81 @@
 //
 // ===========================================================================
 
+#include <Wire.h>
+
 #include "./SoftI2cMaster.h"
+#include "./MCP4728.cpp"
 
 #define SCL_PIN A5
 #define SDA_PIN A4
 #define LDAC_PIN 4
 
-#define CURRENT_DEVICE_ID 0
-#define NEW_DEVICE_ID 0
+#define CURRENT_DEVICE_ID 1
+#define NEW_DEVICE_ID 1
 
 SoftI2cMaster i2c;
+MCP4728 dac;
 
 void setup() {
-    Serial.begin(9600);
-    i2c.init(SCL_PIN, SDA_PIN);
-    pinMode(LDAC_PIN, OUTPUT);
-    writeAddress(CURRENT_DEVICE_ID, NEW_DEVICE_ID);
-    delay(100); // Wait for EEPROM writing 
-    readAddress(); // Read current device ID 
+
+	// Read and write address
+	Serial.begin(9600);
+	i2c.init(SCL_PIN, SDA_PIN);
+	pinMode(LDAC_PIN, OUTPUT);
+	writeAddress(CURRENT_DEVICE_ID, NEW_DEVICE_ID);
+	delay(100);
+	readAddress(); // Read current device ID
+	delay(100);
+
+	// Init I2C communication and DAC for testing
+	Wire.begin();
+	Wire.setClock(400000); // Fast mode
+	dac.init(Wire, NEW_DEVICE_ID, LDAC_PIN);
+	dac.selectVref(MCP4728::VREF::INTERNAL_2_8V, MCP4728::VREF::INTERNAL_2_8V, MCP4728::VREF::INTERNAL_2_8V, MCP4728::VREF::INTERNAL_2_8V);
+	dac.selectPowerDown(MCP4728::PWR_DOWN::NORMAL, MCP4728::PWR_DOWN::NORMAL, MCP4728::PWR_DOWN::NORMAL, MCP4728::PWR_DOWN::NORMAL);
+	dac.selectGain(MCP4728::GAIN::X2, MCP4728::GAIN::X2, MCP4728::GAIN::X2, MCP4728::GAIN::X2);
+	dac.enable(true);
+
 }
 
-void loop() { }
+void loop() { 
+
+	// Test ramp
+	dac.analogWrite(0, 0, 0, 0);
+	delay(2000);
+	for (int i = 0; i < 4000; i++) {
+		dac.analogWrite(i, i, i, i);
+		delayMicroseconds(200);
+	}
+	delay(2000);
+
+}
 
 void readAddress() {
-    digitalWrite(LDAC_PIN, HIGH);
-    i2c.start(0B00000000);
-    i2c.ldacwrite(0B00001100, LDAC_PIN); // Modified command for LDAC latch
-    i2c.restart(0B11000001);
-    uint8_t address = i2c.read(true);
-    i2c.stop();
-    int readAddress = (address & 0B00001110) >> 1;
-    Serial.print("Read address: ");
-    Serial.print(readAddress, DEC);
-    Serial.print(" (");
-    Serial.print(readAddress, BIN);
-    Serial.println(")");
+	digitalWrite(LDAC_PIN, HIGH);
+	i2c.start(0B00000000);
+	i2c.ldacwrite(0B00001100, LDAC_PIN); // Modified command for LDAC latch
+	i2c.restart(0B11000001);
+	uint8_t address = i2c.read(true);
+	i2c.stop();
+	int readAddress = (address & 0B00001110) >> 1;
+	Serial.print("Read address: ");
+	Serial.print(readAddress, DEC);
+	Serial.print(" (");
+	Serial.print(readAddress, BIN);
+	Serial.println(")");
 }
 
 void writeAddress(int oldAddress, int newAddress) {
-    Serial.print("Writing address: ");
-    Serial.print(newAddress, DEC);
-    Serial.print(" (");
-    Serial.print(newAddress, BIN);
-    Serial.println(")");
-    digitalWrite(LDAC_PIN, HIGH);
-    i2c.start(0B11000000 | (oldAddress << 1));
-    i2c.ldacwrite(0B01100001 | (oldAddress << 2), LDAC_PIN); // Modified command for LDAC latch
-    i2c.write(0B01100010 | (newAddress << 2));
-    i2c.write(0B01100011 | (newAddress << 2));
-    i2c.stop();
+	Serial.print("Writing address: ");
+	Serial.print(newAddress, DEC);
+	Serial.print(" (");
+	Serial.print(newAddress, BIN);
+	Serial.println(")");
+	digitalWrite(LDAC_PIN, HIGH);
+	i2c.start(0B11000000 | (oldAddress << 1));
+	i2c.ldacwrite(0B01100001 | (oldAddress << 2), LDAC_PIN); // Modified command for LDAC latch
+	i2c.write(0B01100010 | (newAddress << 2));
+	i2c.write(0B01100011 | (newAddress << 2));
+	i2c.stop();
 }
